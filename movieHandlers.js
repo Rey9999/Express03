f9c3ce81fa746d59a6f9daa69a79b09b493a4279
PRUDENT Reynald (SNCF VOYAGEURS / Direction Intercites / EQUIPE 1 ESCALE PAZ) <8808197c@commun.ad.sncf.fr>
2022-08-24 10:37:14 +0200

    validey4

--- movieHandlers.js
+++ movieHandlers.js
@@ -27,8 +27,16 @@ const movies = [{
 ];
 
 const getMovies = (req, res) => {
+    let sql = "select * from movies";
+    const sqlValues = [];
+
+    if (req.query.color != null) {
+        sql += " where color = ?";
+        sqlValues.push(req.query.color);
+    }
+
     database
-        .query("select * from movies")
+        .query(sql, sqlValues)
         .then(([movies]) => {
             res.json(movies);
         })
@@ -42,46 +50,44 @@ const getMovieById = (req, res) => {
     const id = parseInt(req.params.id);
 
     const movie = movies.find((movie) => movie.id === id);
-   database
-    .query("select * from movies where id = ?", [id])
-    .then(([movies]) => {
-      if (movies[0] != null) {
-        res.json(movies[0]);
-      } else {
-        res.status(404).send("Not Found");
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error retrieving data from database");
-    });
+    database
+        .query("select * from movies where id = ?", [id])
+        .then(([movies]) => {
+            if (movies[0] != null) {
+                res.json(movies[0]);
+            } else {
+                res.status(404).send("Not Found");
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error retrieving data from database");
+        });
 };
 
 // in movieHandlers.js
 
 const postMovie = (req, res) => {
-  const { title, director, year, color, duration } = req.body;
+    const { title, director, year, color, duration } = req.body;
 
-  database
-    .query(
-      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
-      [title, director, year, color, duration]
-    )
-    .then(([result]) => {
-      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error saving the movie");
-    });
+    database
+        .query(
+            "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)", [title, director, year, color, duration]
+        )
+        .then(([result]) => {
+            res.location(`/api/movies/${result.insertId}`).sendStatus(201);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error saving the movie");
+        });
 };
 
 
 
 module.exports = {
-  getMovies,
-  getMovieById,
-  postMovie,
- // don't forget to export your function ;)
+    getMovies,
+    getMovieById,
+    postMovie,
+    // don't forget to export your function ;)
 };
-
--- users.js
+++ users.js
@@ -1,15 +1,33 @@
 const database = require("./database");
+
 const getUser = (req, res) => {
+    let sql = "select * from users";
+    const sqlValues = [];
+
+    if (req.query.language != null) {
+        sql += " where language = ?";
+        sqlValues.push(req.query.language);
+
+        if (req.query.city != null) {
+            sql += " and city = ?";
+            sqlValues.push(req.query.city);
+        }
+    } else if (req.query.city != null) {
+        sql += " where city = ?";
+        sqlValues.push(req.query.city);
+    }
+
     database
-        .query("select * from users")
-        .then(([users]) => {
-            res.json(users);
+        .query(sql, sqlValues)
+        .then(([user]) => {
+            res.json(user);
         })
         .catch((err) => {
             console.error(err);
             res.status(500).send("Error retrieving data from database");
         });
 };
+
 const getUserById = (req, res) => {
     const id = parseInt(req.params.id);
     database
@@ -28,7 +46,6 @@ const getUserById = (req, res) => {
 };
 
 const getNewUser = (req, res) => {
-
     const { firstname, lastname, email, city, language } = req.body;
 
     database
@@ -42,9 +59,7 @@ const getNewUser = (req, res) => {
             console.error(err);
             res.status(500).send("Error saving the user");
         });
-
-
-}
+};
 
 const updateUser = (req, res) => {
     const id = parseInt(req.params.id);
@@ -86,11 +101,10 @@ const deleteUser = (req, res) => {
         });
 };
 
-
 module.exports = {
     getUser,
     getUserById,
     getNewUser,
     updateUser,
-    deleteUser
+    deleteUser,
 };
